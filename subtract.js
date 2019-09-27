d7d5234a23ea549e4009fef5694f20c0de0fae10
kvmohan <vamsi_kvm@yahoo.com>
2019-09-28 00:12:16 +0530

    chages committed to subtract.js

--- subtract.js
+++ subtract.js
@@ -1,11 +1,8 @@
 //subtract
-<<<<<<< HEAD
 //return subtraction of two numbers
 //Test line for Stash function
 // Adding new line
-=======
-//Adding new line
->>>>>>> testing-2
+
 function sutract(a,b,bMINUSa){
     if(bMINUSa === true) {
 	return b - a;
